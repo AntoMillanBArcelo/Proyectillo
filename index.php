6ca53b9c0ff8ad9efb258620da01b956d83abb78
Antonio <amilbar951@g.educaand.es>
2023-10-30 09:37:38 +0100

    Creacion de examenes y preguntas

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
 include 'metodos/metodoPintaMenu.php';
 crearMenu(); 
+echo '<li><a href="/plantilla/plantillaPregunta.html">Iniciar Sesión</a></li>';
+echo '<li><a href="/plantilla/examen.html">Iniciar Sesión</a></li>';
 
