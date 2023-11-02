3d5b98acc83fad2f1f3c4373dca652c30765cffd
Antonio Millán <amilbar951@g.educaand.es>
2023-11-02 16:25:22 +0100

    index css

--- index.php
+++ index.php
@@ -1,9 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Autoescuela</title>
+    <link rel="stylesheet" href="css/cssIndex.css">
+</head>
+<body>
+    <video id="video-background" autoplay="autoplay" muted="muted" loop="loop">
+        <source src="img/Forza_Net_Splash_Page_Motorsport_adb7e0d56b.mp4" type="video/mp4">
+       
+    </video>
+
+    <nav>
+        <ul>
+            <li><a href="index.html">Inicio</a></li>
+            <li><a href="login.php">Iniciar sesión</a></li>
+            <li><a href="logout.php">Cerrar sesión</a></li>
+        </ul>
+    </nav>
+  
+    <h1>NOS VEMOS EN LA <br>LÍNEA DE SALIDA</h1> 
+    
+    <footer>© 2023 Antonio Millán</footer>
+</body>
+</html>
+
 <?php
 include_once 'metodos/MetodoPintaMenu.php';
 include_once 'db/db.php';
 include 'clases/Usuario.php';
 // Llama a la función crearMenu de la clase MetodoPintaMenu para generar el menú.
-MetodoPintaMenu::crearMenu();
 $con = db::obtenerConexion();
 session_start();
 
